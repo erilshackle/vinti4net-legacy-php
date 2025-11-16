4c42cfeec070de4ed53e7b09edbaca94b4128bb1
Eril Shackle <erilandocarvalho@gmail.com>
2025-11-16 19:59:05 -0100

    Add PHPUnit configuration and bootstrap; update .gitignore

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+// Autoload do Composer (se você instalou dependências via composer)
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Ou, se não usa Composer, inclua a classe manualmente:
+require_once __DIR__ . '/../src/Vinti4NetLegacy.php';
